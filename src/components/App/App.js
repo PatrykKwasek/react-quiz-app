25c766a53ae5e6019e21900207e7ab3da48c42a2
patryk.kwasek <patryk.kwasek@accenture.com>
2020-07-09 13:51:05 +0200

    Last update

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import WelcomePage from "../WelcomePage/WelcomePage";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <WelcomePage />
+            </div>
+        )
+    }
+}
+
+export default App;
